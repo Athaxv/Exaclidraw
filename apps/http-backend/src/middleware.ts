dcf62ebcb7100d88d12ab3913be6c9d4f072b446
AtharvGaur <atharava112@gmail.com>
2025-08-27 00:18:04 +0530

    minor changes

---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "./config";
+
+// Extend Express Request interface to include userId
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string;
+    }
+  }
+}
+
+export async function middleware(req: Request, res: Response, next: NextFunction){
+    const token = req.headers["authorization"] ?? "";
+
+    const decode = jwt.verify(token, JWT_SECRET)
+
+    if (decode){
+      // @ts-ignore
+        req.userId = decode.userId;
+        next();
+    }
+    else {
+        res.status(403).json({
+            message: "Unauthorized"
+        })
+    }
+}
