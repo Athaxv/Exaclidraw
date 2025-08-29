db984b857c976dc463639f100b0899c0dc9e2982
AtharvGaur <atharava112@gmail.com>
2025-08-30 00:10:06 +0530

    feat(common): add initial package.json, tsconfig.json, and type schemas

---
+++ packages/common/src/types.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const createUserSchema = z.object({
+    username: z.string().min(3).max(20),
+    password: z.string().min(3).max(20),
+    name: z.string()
+})
+export const signinSchema = z.object({
+    username: z.string().min(3).max(20),
+    password: z.string().min(3).max(20),
+})
+export const createRoomSchema = z.object({
+    username: z.string().min(3).max(20),
+})
+
