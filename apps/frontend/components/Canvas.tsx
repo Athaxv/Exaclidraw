a5d6d3dd4c7fc44baea67a5c13c57d2ba012f438
AtharvGaur <atharava112@gmail.com>
2025-09-20 00:17:14 +0530

    feat: enhance styling and layout with Tailwind CSS, add ThemeProvider, and implement shape selection in Canvas

--- apps/frontend/components/Canvas.tsx
+++ apps/frontend/components/Canvas.tsx
@@ -1,8 +1,17 @@
 import { initDraw } from "@/draw";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Dockbar } from "./Dockbar";
+
+type Shape = "rect" | "Ellipse" | "Diamond" | "circle" | "pencil";
 
 export function Canvas({ roomId, socket }: { roomId: string, socket: WebSocket }){
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [ selectedShape, setSelectedShape ] = useState<Shape>("rect");
+
+    useEffect(() => {
+        //@ts-ignore
+        window.selectedShape = selectedShape
+    }, [selectedShape])
 
     useEffect(() => {
         if (canvasRef.current){
@@ -17,7 +26,18 @@ export function Canvas({ roomId, socket }: { roomId: string, socket: WebSocket }
         }
     }, [canvasRef, roomId])
 
-    return <div className="w-full h-full">
-        <canvas height="730" ref={canvasRef}  width="1500"></canvas>
+    return <div className="flex flex-col w-full h-full">
+    <div className="flex-1 flex items-center justify-center relative w-full h-full">
+      {/* Dockbar overlayed inside canvas area */}
+      <div className="absolute top-4 z-10 px-4 py-2 flex">
+        <Dockbar selectedShape={selectedShape} setSelectedShape={setSelectedShape}/>
+      </div>
+      <canvas
+        height="730"
+        ref={canvasRef}
+        width="1500"
+        className="w-full h-full"
+      ></canvas>
     </div>
+  </div>
 }
--- apps/frontend/draw/index.ts
+++ apps/frontend/draw/index.ts
@@ -12,13 +12,19 @@ type Shape = {
     centerX: number;
     centerY: number;
     radius: number
+} | {
+    type: "pencil";
+    startX: number;
+    startY: number;
+    endX: number;
+    endY: number;
 }
 
 export async function initDraw(canvas: HTMLCanvasElement, roomId: string, socket: WebSocket) {
     const ctx = canvas.getContext("2d");
     const existingShapes: Shape[] = await getExistingShapes(roomId);
 
-    console.log(existingShapes)
+    // console.log(existingShapes)
 
     if (!ctx) {
         throw new Error("Failed to get 2D context from canvas.");
@@ -27,10 +33,10 @@ export async function initDraw(canvas: HTMLCanvasElement, roomId: string, socket
     socket.onmessage = (event) => {
         const message = JSON.parse(event.data);
 
-        if (message.type === "chat"){
+        if (message.type === "chat") {
             const parsedChat = JSON.parse(message.message);
             existingShapes.push(parsedChat.shape)
-            clearCanvas(canvas, existingShapes,ctx);
+            clearCanvas(canvas, existingShapes, ctx);
         }
     }
 
@@ -48,13 +54,32 @@ export async function initDraw(canvas: HTMLCanvasElement, roomId: string, socket
         clicked = false;
         const width = e.clientX - startX;
         const height = e.clientY - startY;
-        const shape: Shape = {
-            type: "rect",
-            x: startX,
-            y: startY,
-            height,
-            width
-        };
+
+        //@ts-ignore
+        const selectedShape = window.selectedShape;
+        let shape: Shape | null = null;
+        if (selectedShape === "rect") {
+            shape = {
+                type: "rect",
+                x: startX,
+                y: startY,
+                height,
+                width
+            };
+        } else if (selectedShape === "circle") {
+            const radius = Math.abs(Math.max(width, height) / 2);
+            shape = {
+                type: "circle",
+                centerX: startX + radius,
+                centerY: startY + radius,
+                radius: radius
+            };
+        }
+        console.log("shape", shape);
+        if (!shape){
+            return;
+        }
+
         existingShapes.push(shape);
 
         socket.send(JSON.stringify({
@@ -71,29 +96,48 @@ export async function initDraw(canvas: HTMLCanvasElement, roomId: string, socket
             const height = e.clientY - startY;
             clearCanvas(canvas, existingShapes, ctx);
             ctx.strokeStyle = "rgba(255, 255, 255, 1)";
-            ctx.strokeRect(startX, startY, width, height);
+            //@ts-ignore
+            const selectedShape = window.selectedShape;
+            if (selectedShape === "rect") {
+                ctx.strokeRect(startX, startY, width, height);
+            }
+            else if (selectedShape === "circle") {
+                const radius = Math.max(width, height) / 2;
+                const centerX = startX + radius;
+                const centerY = startY + radius;
+                ctx.beginPath();
+                ctx.arc(centerX, centerY, Math.abs(radius), 0, Math.PI * 2);
+                ctx.stroke();
+                ctx.closePath();
+            }
         }
     })
 }
 
-function clearCanvas(canvas: HTMLCanvasElement, existingShapes: Shape[], ctx: CanvasRenderingContext2D){
+function clearCanvas(canvas: HTMLCanvasElement, existingShapes: Shape[], ctx: CanvasRenderingContext2D) {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.fillStyle = "rgba(0, 0, 0)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     existingShapes.map((shape) => {
-        if (shape.type === "rect"){
+        if (shape.type === "rect") {
             ctx.strokeStyle = "rgba(255, 255, 255)";
             ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+        }
+        else if (shape.type === "circle") {
+            ctx.beginPath();
+            ctx.arc(shape.centerX, shape.centerY, shape.radius, 0, Math.PI * 2);
+            ctx.stroke();
+            ctx.closePath();
         }
     })
 }
 
-async function getExistingShapes(roomId: string){
+async function getExistingShapes(roomId: string) {
     const res = await axios.get(`${HTTP_BACKEND}/chat/${roomId}`)
     const messages = res.data.messages;
 
-    const shapes = messages.map((x: {message: string}) => {
+    const shapes = messages.map((x: { message: string }) => {
         const msgData = JSON.parse(x.message)
         return msgData.shape;
     })
