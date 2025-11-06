4c90b82bc5d116d4fb0ae358307fdb2cc933fdb5
AtharvGaur <atharava112@gmail.com>
2025-11-06 18:37:08 +0530

    chore: update package exports and Prisma configuration
    - Added exports for `package.json` in `backend-common`, `common`, and `db-package` to improve module accessibility.
    - Updated Prisma configuration in generated files to include `schemaEnvPath` for better environment management.
    - Adjusted paths in Prisma scripts to simplify file references.

--- packages/db-package/generated/prisma/index.js
+++ packages/db-package/generated/prisma/index.js
@@ -165,7 +165,8 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null
+    "rootEnvPath": null,
+    "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
   "clientVersion": "6.15.0",
@@ -193,8 +194,8 @@ const fs = require('fs')
 config.dirname = __dirname
 if (!fs.existsSync(path.join(__dirname, 'schema.prisma'))) {
   const alternativePaths = [
-    "../../packages/db-package/generated/prisma",
-    "../packages/db-package/generated/prisma",
+    "generated/prisma",
+    "prisma",
   ]
   
   const alternativePath = alternativePaths.find((altPath) => {
@@ -224,7 +225,7 @@ Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
 path.join(__dirname, "query_engine-windows.dll.node");
-path.join(process.cwd(), "../../packages/db-package/generated/prisma/query_engine-windows.dll.node")
+path.join(process.cwd(), "generated/prisma/query_engine-windows.dll.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
-path.join(process.cwd(), "../../packages/db-package/generated/prisma/schema.prisma")
+path.join(process.cwd(), "generated/prisma/schema.prisma")
